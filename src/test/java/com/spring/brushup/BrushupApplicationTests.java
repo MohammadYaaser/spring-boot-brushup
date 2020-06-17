69ba38be4949480820081392d98e772227fb805c
MohammadYaaser <MohammadYaseer@.github.com>
2020-06-17 10:25:45 +0530

    Spring boot starter created

---
+++ src/test/java/com/spring/brushup/BrushupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.brushup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrushupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
